df8d545610048f41a313d5244a588c9371382b3e
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-27 12:37:27 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioFuncaoSenac/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Primeiro número: ");
+        int x = int.Parse(Console.ReadLine());
+
+        Console.Write("Segundo número: ");
+        int y = int.Parse(Console.ReadLine());
+
+        int resultado = Soma(x, y);
+        Console.WriteLine("Soma ={0}", resultado);
+
+    }
+    static int Soma(int n1, int n2)
+    {
+        int s = n1 + n2;
+        return s;
+    }
+}
